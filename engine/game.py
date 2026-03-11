3e6b02028f019075e8d1d37080b0254dd8933de8
Schmorv <imarvinweyermann@gmail.com>
2026-03-11 18:53:01 +0100

    viele neue features mieser bug mit rows,cols bei resize

--- engine/game.py
+++ engine/game.py
@@ -1,27 +1,88 @@
+from math import gamma
+
 import numpy as np
 
 class GameOfLife:
-    def __init__(self,rows: int, cols: int, ) -> None:
-        self.board = np.zeros((rows, cols), dtype=bool)
+    _dtype = bool
+
+    def __init__(self,rows: int = 30, cols: int =30, random_start: bool = True, random_ones: int =None):
+        self.curr_board = np.zeros((rows, cols), dtype=self._dtype)
+        if random_start:
+            self.curr_board = create_random_board(rows=rows, cols=cols,type_value = self._dtype, random_ones=random_ones)
         self.rows = rows
         self.cols = cols
+        self.history = list[np.ndarray]()
+        self.generation = 0
+        self.random_start = random_start
 
-    def __str__(self) -> str:
-        return self.board.__str__()
+    def change_size(self, new_rows: int, new_cols : int) -> None:
+        if new_rows == self.rows and new_cols == self.cols:
+            return
 
-def create_board(n: int) -> np.ndarray:
-    board = np.zeros((n, n), dtype=int)
-    return board
+        new = np.zeros((new_rows, new_cols), dtype=self._dtype)
 
-def create_random_board(n: int, random_ones:int) -> np.ndarray:
-    board = np.zeros((n, n), dtype=int)
-    for i in range(random_ones):
-        row, col = np.random.random_integers(0, n - 1), np.random.random_integers(0, n - 1)
-        if board[row, col] == 0:
-            board[row, col] = 1
-        else:
-            i -= 1
-    return board
+        min_rows = min(self.rows, new_rows)
+        min_cols = min(self.cols, new_cols)
+
+        new[0:min_rows, 0:min_cols] = self.curr_board[0:min_rows, 0:min_cols].copy()
+
+        self.curr_board = new
+        self.rows = new_rows
+        self.cols = new_cols
+
+
+    @classmethod
+    def create_from_board(cls, board: np.ndarray) -> GameOfLife:
+        obj = cls()
+        obj.curr_board = board
+        obj.rows, obj.cols = board.shape
+        return obj
+
+    def next_generation(self) -> None:
+        next_board = np.zeros((self.rows,self.cols), dtype=self._dtype)
+        for idx in range(0, next_board.shape[0]):
+            for idy in range(0, next_board.shape[1]):
+                next_board[idx, idy] = self.is_cell_alive_next_gen(idx, idy)
+        self.history.append(self.curr_board)
+        self.curr_board = next_board
+        self.generation += 1
+
+    def is_cell_alive_next_gen(self, idx: int, idy: int) -> int:
+        living_neighbours = self.count_living_neighbours(idx, idy)
+        if living_neighbours == 2:
+            return self.curr_board[idx, idy]
+        elif living_neighbours == 3:
+            return 1
+        return 0
+
+    def count_living_neighbours(self,idx: int, idy: int) -> int:
+        # neigbours indices
+        left_idx = idx - 1 if idx > 0 else 0
+        right_idx = idx + 1 if idx + 1 < self.rows else idx
+        up_idy = idy - 1 if idy > 0 else 0
+        down_idy = idy + 1 if idy + 1 < self.cols else idy
+        living_neighbours = 0
+        for x in range(left_idx, right_idx + 1):
+            for y in range(up_idy, down_idy + 1):
+                if x != idx or y != idy:
+                    if self.curr_board[x, y] == 1:
+                        living_neighbours += 1
+        return living_neighbours
+
+    def last_generation(self) -> None:
+        if self.generation == 0:
+            return
+        self.generation -= 1
+        self.curr_board = self.history.pop()
+
+    def clear_board(self) -> None:
+        self.curr_board = np.zeros((self.rows, self.cols), dtype=self._dtype)
+        self.generation = 0
+        self.history.clear()
+
+    def reset_board(self) -> None:
+        self.clear_board()
+        self.curr_board = create_random_board(rows=self.rows, cols=self.cols, type_value=self._dtype)
 
 def print_board(board: np.ndarray) -> None:
     rows, cols = board.shape
@@ -29,38 +90,19 @@ def print_board(board: np.ndarray) -> None:
         for y in range(0, cols):
             a = board[x, y]
             print(a, " ", end="")
-        print()
+        print(board.dtype)
     return
 
-
-def next_generation(curr_board: np.ndarray) -> np.ndarray:
-    next_board = np.zeros(curr_board.shape, dtype=int)
-    for idx in range(0, next_board.shape[0]):
-        for idy in range(0, next_board.shape[1]):
-            next_board[idx, idy] = is_cell_alive_next_gen(curr_board, idx, idy)
-    return next_board
-
-
-def is_cell_alive_next_gen(board: np.ndarray, idx: int, idy: int) -> int:
-    living_neighbours = count_living_neighbours(board, idx, idy)
-    if living_neighbours == 2:
-        return board[idx, idy]
-    elif living_neighbours == 3:
-        return 1
-    return 0
-
-
-def count_living_neighbours(board: np.ndarray, idx: int, idy: int) -> int:
-    # neigbours indices
-    left_idx = idx - 1 if idx > 0 else 0
-    right_idx = idx + 1 if idx + 1 < board.shape[0] else idx
-    up_idy = idy - 1 if idy > 0 else 0
-    down_idy = idy + 1 if idy + 1 < board.shape[1] else idy
-
-    living_neighbours = 0
-    for x in range(left_idx, right_idx + 1):
-        for y in range(up_idy, down_idy + 1):
-            if x != idx or y != idy:
-                if board[x, y] == 1:
-                    living_neighbours += 1
-    return living_neighbours
+def create_random_board(rows: int, cols:  int, type_value: type , random_ones: int = None):
+    if not random_ones:
+        random_ones = rows * cols//3
+    if not 0 < random_ones <= (rows * cols):
+        raise ValueError('random_ones must be between 0 and rows * cols')
+    board = np.zeros((rows, cols), dtype=type_value)
+    for i in range(random_ones):
+        row, col = np.random.randint(0, rows), np.random.randint(0, cols)
+        if board[row, col] == 0:
+            board[row, col] = 1
+        else:
+            i -= 1
+    return board
--- gui/app.py
+++ gui/app.py
@@ -1,32 +1,64 @@
+import time
+
 import dearpygui.dearpygui as dpg
 import numpy as np
 
-from engine import game as eng
+from engine.game import GameOfLife
+
+DRAWLIST_TAG = "drawlist_board"
+BOARD_WIDTH = 200
+BOARD_HEIGHT = 200
 
 def draw_board(board: np.ndarray, width: int, height: int) -> None:
     cell_width = float(width) / board.shape[0]
     cell_height = float(height) / board.shape[1]
-    print(width, height)
-    print(board)
-    with dpg.drawlist(tag="drawlist_board",width=width, height=height):
-        dpg.draw_text((0, 0), "Origin", color=(0, 0, 0, 0), size=15)
-        dpg.draw_rectangle((0,0), (width,height), color=(255,255,255), fill=(255,255,255))
-        living_cells = np.where(board == 1)
-        print(living_cells)
-        #living_cells = np.array((living_cells[0],living_cells[1]))
-        living_cells = np.column_stack((living_cells[1], living_cells[0]))
-        for row, col in living_cells:
-            dpg.draw_rectangle((cell_width*row, cell_height*col), (cell_width*(row+1), cell_height*(col+1)), color=(0, 0, 0), fill=(0, 0, 0))
-        for row in range(board.shape[0]+1):
-            dpg.draw_line((cell_width * row, 0), (cell_width * row, height), color=(0, 0, 0))
-        for col in range(board.shape[1] + 1):
-            dpg.draw_line((0,cell_height*col), (width,cell_height*col), color=(0, 0, 0))
+    #print(board)
+    dpg.delete_item(DRAWLIST_TAG, children_only=True)
+    dpg.draw_rectangle((0,0), (width,height), color=(255,255,255), fill=(255,255,255), parent=DRAWLIST_TAG)
+    living_cells = np.where(board == True)
+    living_cells = np.column_stack((living_cells[1], living_cells[0]))
+    for row, col in living_cells:
+        dpg.draw_rectangle((cell_width*row, cell_height*col), (cell_width*(row+1), cell_height*(col+1)), color=(0, 0, 0), fill=(0, 0, 0),parent=DRAWLIST_TAG)
+    for row in range(board.shape[0]+1):
+        dpg.draw_line((cell_width * row, 0), (cell_width * row, height), color=(0, 0, 0),parent=DRAWLIST_TAG)
+    for col in range(board.shape[1] + 1):
+        dpg.draw_line((0,cell_height*col), (width,cell_height*col), color=(0, 0, 0),parent=DRAWLIST_TAG)
+
+def draw_last_board(sender, app_data, user_data):
+    user_data.last_generation()
+    draw_board(user_data.curr_board, width=BOARD_HEIGHT, height=BOARD_HEIGHT)
+
+def draw_next_board(sender, app_data, user_data):
+    user_data.next_generation()
+    draw_board(user_data.curr_board, width=BOARD_HEIGHT, height=BOARD_HEIGHT)
+
+def run_game(sender, app_data, user_data):
+    while dpg.get_value(sender):
+        user_data.next_generation()
+        draw_board(user_data.curr_board, width=BOARD_HEIGHT, height=BOARD_HEIGHT)
+        time.sleep(0.1)
+
+def clear_board(sender, app_data, user_data):
+    user_data.clear_board()
+    draw_board(user_data.curr_board, width=BOARD_HEIGHT, height=BOARD_HEIGHT)
+
+def random_reset(sender, app_data, user_data):
+    user_data.reset_board()
+    draw_board(user_data.curr_board, width=BOARD_HEIGHT, height=BOARD_HEIGHT)
+    print(user_data.curr_board.shape, user_data.rows, user_data.cols)
+
+def new_board(sender, app_data, user_data):
+    new_rows = dpg.get_value("input_rows")
+    new_cols = dpg.get_value("input_cols")
+    user_data.change_size(new_rows, new_cols)
+    draw_board(user_data.curr_board, width=BOARD_HEIGHT, height=BOARD_HEIGHT)
+    print(user_data.rows, user_data.cols)
 
 def main():
     # always first command
     dpg.create_context()
 
-    board: np.ndarray = eng.create_random_board(10,30)
+    game: GameOfLife = GameOfLife(8,5)
 
 
     with dpg.window(tag="Primary Window"):
@@ -38,17 +70,25 @@ def main():
         with dpg.group(horizontal=True):
             # Links: Spielfeld-Container
             with dpg.child_window(width=300, height=300, border=True):
-                draw_board(board, 200-50, 300-50)
+                #BOARD_WIDTH = dpg.get_item_parent()
+                dpg.add_drawlist(tag=DRAWLIST_TAG, width=BOARD_WIDTH, height=BOARD_HEIGHT)
+                draw_board(game.curr_board, width=BOARD_WIDTH, height=BOARD_HEIGHT)
 
             # Rechts: Input-Container
-            with dpg.child_window(width=0, height=200, border=True):  # width=0 -> nimmt Restbreite
+            with dpg.child_window(width=200, height=200, border=True):  # width=0 -> nimmt Restbreite
                 dpg.add_text("CONTROLS")
                 dpg.add_separator()
-                dpg.add_input_int(label="Rows", default_value=50, min_value=1)
-                dpg.add_input_int(label="Cols", default_value=50, min_value=1)
-                dpg.add_button(label="Reset")
-                dpg.add_button(label="Step")
-                dpg.add_checkbox(label="Run", default_value=False)
+                dpg.add_input_int(tag="input_rows",label="Rows", default_value=20, min_value=1,min_clamped=True)
+                dpg.add_input_int(tag="input_cols",label="Cols", default_value=20, min_value=1,min_clamped=True)
+                dpg.add_button(label="new Board", callback=new_board, user_data=game)
+                dpg.add_separator()
+                with dpg.group(horizontal=True):
+                    dpg.add_button(label="Clear Board",callback=clear_board,user_data=game)
+                    dpg.add_button(label="Random Reset",callback=random_reset,user_data=game)
+                with dpg.group(horizontal=True):
+                    dpg.add_button(tag="btn_step_back",label="<",callback=draw_last_board,user_data=game)
+                    dpg.add_button(tag="btn_board_forward",label=">",callback=draw_next_board,user_data=game)
+                dpg.add_checkbox(label="Run", default_value=False,callback=run_game,user_data=game)
 
     #window created by operating System
     dpg.create_viewport(title='Schmorv\'s Game of Life', width=600, height=600)
